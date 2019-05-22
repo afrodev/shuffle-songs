f297cffe5e7180e8a2e0e6d28e0fb7b2a890186d
Humberto Vieira <humbertovieira.castro@gmail.com>
2019-05-22 12:50:27 -0300

    project initiated

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # shuffle-songs
-Test for ThoughtWorks 
+
+# Project Dependencies
+- SnapKit 5.0.1 (Used to do the UI components in ViewCode)
+- Quick 2.1.0 (Used to do tests)
+- Nimble 8.0.1 (Used to do tests)
+
+
---
+++ shuffle-songs/shuffle-songs/Features/ShuffleSongs/ViewController/SongsVC.swift
@@ -0,0 +1,30 @@
+//
+//  SongsVC.swift
+//  shuffle-songs
+//
+//  Created by Humberto Vieira on 22/05/19.
+//  Copyright © 2019 Humberto Vieira. All rights reserved.
+//
+
+import UIKit
+
+class SongsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
